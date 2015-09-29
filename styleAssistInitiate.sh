8b12f0d42eca44ec8542b4ab59c48ca44d3f4fff
chrisrolson <chrisrolson@me.com>
2015-09-29 11:22:17 -0500

    Update styleAssistInitiate.sh
    added function to remove script file after installation. Corrected minor syntax errors with scripts.

--- styleAssistInitiate.sh
+++ styleAssistInitiate.sh
@@ -4,9 +4,10 @@
 #################################################
 
 ## Style Assist HTML5 Install (Cleanly install SA components)
-sudo /Applications/Adobe\ Extension\ Manager\ CC/Adobe\ Extension\ Manager\ CC.app/Contents/MacOS/Adobe\ Extension\ Manager\ CC -suppress -install zxp="/Users/Shared/Scripts/styleAssistCCHTML5.zxp"
+##sudo /Applications/Adobe\ Extension\ Manager\ CC/Adobe\ Extension\ Manager\ CC.app/Contents/MacOS/Adobe\ Extension\ Manager\ CC -suppress -install zxp="/Users/Shared/Scripts/styleAssistCCHTML5.zxp"
 
-sudo rm -Rf /Users/Shared/Scripts/style*
-sudo rm -rf /Applications/Adobe\ InDesign\ CC\ 2014/Plug-Ins/IDHostAdapter.InDesignPlugin
+sudo /Applications/Adobe\ Extension\ Manager\ CC/Adobe\ Extension\ Manager\ CC.app/Contents/MacOS/ExManCmd --install /Users/Shared/Scripts/styleAssistCCHTML5.zxp
 
+
+sudo rm -Rf /Users/Shared/Scripts/style
 #################################################
